2dd20846c990fde51c5e0b38de0d3fd30d8d4d7b
Thitiwatdb <thitiwat.db@gmail.com>
2025-03-15 10:48:51 +0700

    patch 1.4.2

--- frontend/src/pages/dashboard/UploadImages.jsx
+++ frontend/src/pages/dashboard/UploadImages.jsx
@@ -21,16 +21,17 @@ export default function UploadImages() {
   return (
     <>
       <div className=" flex flex-col items-center justify-center mt-10">
-        <div className="p-4 bg-blue-400 shadow-md rounded-md">
+        <h1 className="text-2xl m-5 font-bold text-blue-500">Upload Images</h1>
+        <div className="p-4 bg-blue-200 shadow-md rounded-md ">
           <input
             type="file"
             multiple
             accept="image/*"
             onChange={onImageChange}
-            className="p-4 shadow-md rounded-md bg-white"
+            className="p-4 shadow-md rounded-md bg-white text-blue-500"
           />
         </div>
-        <div className="p-4 bg-white shadow-md rounded-md w-2xl mt-4">
+        <div className="p-10 bg-white shadow-md rounded-md w-96 mt-4">
           {imagesURL.map((imageURL, index) => (
             <img key={index} src={imageURL} alt="preview" />
           ))}
